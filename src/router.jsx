3ea76cde5bbb3c16f52c1c80b37608cba3e69b48
yeeun426 <thsudkcla7@naver.com>
2025-04-25 18:00:19 +0900

    fix : 404 error

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainPage />,
+    },
+  ],
   {
-    path: '/',
-    children: [{ path: '', element: <MainPage /> }],
-  },
-])
+    basename: '/financial-ledger-ureca',
+  }
+)
 
 export default router
